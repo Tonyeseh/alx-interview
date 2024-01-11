e19d8ed40f46266b85ae62f82d50c6ed60c94423
Tonyeseh <esehtony123@gmail.com>
2024-01-11 14:12:49 +0100

    task 0 0x01

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""0-lockboxes"""
+
+
+def canUnlockAll(boxes: list[list[int]]) -> bool:
+    """
+    You have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1 and
+    each box may contain keys to the other boxes.
+    The first box boxes[0] is unlocked
+
+    determines if all the boxes can be opened.
+
+    boxes: is a list of lists
+
+    Return True if all boxes can be opened, else return False
+    """
+    keys: list[int] = [0, ]
+
+    for box in range(len(boxes)):
+        if box not in keys:
+            return False
+        keys += boxes[box]
+    return True
