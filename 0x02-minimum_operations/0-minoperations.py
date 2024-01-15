ec22b4f7322e508cb6efd9dbcede4ac5392af8f7
Tonyeseh <esehtony123@gmail.com>
2024-01-15 13:31:40 +0100

    task 0 0x02

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""0-minoperations"""
+
+def minOperations(n):
+    """calculates the fewest number of operations needed to
+    result in exactly n H characters in the file."""
+
+    factors = []
+
+    if n <= 1:
+        return 0
+    
+    for i in range(2, int(n ** .5) + 1):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+
+    if n > 1:
+        factors.append(n)
+
+    return sum(factors)
