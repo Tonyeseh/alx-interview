cbf15de9b8283d89564fb80a12370e80a81024c4
Tonyeseh <esehtony123@gmail.com>
2024-01-16 14:36:34 +0100

    fixed lint errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """0-minoperations"""
 
+
 def minOperations(n):
     """calculates the fewest number of operations needed to
     result in exactly n H characters in the file."""
@@ -9,7 +10,7 @@ def minOperations(n):
 
     if n <= 1:
         return 0
-    
+
     for i in range(2, int(n ** .5) + 1):
         while n % i == 0:
             factors.append(i)
