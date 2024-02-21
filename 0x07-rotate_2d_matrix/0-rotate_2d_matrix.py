69dae457d2e2fbc2c591eabc217788d16caddff6
Tonyeseh <esehtony123@gmail.com>
2024-02-21 22:25:03 +0100

    task 0 0x07

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""0-rotate_2d_matrix"""
+
+def rotate_2d_matrix(matrix):
+    """Rotate a 2d matrix 90 degrees clockwise"""
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix) // 2):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][len(matrix) - 1 - j]
+            matrix[i][len(matrix) - 1 - j] = temp
