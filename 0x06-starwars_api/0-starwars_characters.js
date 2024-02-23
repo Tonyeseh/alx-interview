d21a423833cb12ded3b6e818cb30269991241fee
Tonyeseh <esehtony123@gmail.com>
2024-02-23 09:51:45 +0100

    task 0 0x06

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,27 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
+const argv = process.argv;
 
-resp = request('https://swapi-api.alx-tools.com/api/films/1', function (error, response, body) {
-    console.error('error:', error)
-    console.log('response:', response)
-    console.log('body:', body)
-})
+const route = 'https://swapi-api.alx-tools.com/api/films/';
+
+resp = request(`${route}${argv[2]}`, function (error, response, body) {
+  if (!error) {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    printCharacterName(characters, 0);
+  }
+});
+
+function printCharacterName(characters, idx) {
+  request.get(characters[idx], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (idx + 1 < characters.length) {
+        printCharacterName(characters, idx + 1);
+      }
+    } else {
+      console.log(error);
+    }
+  });
+}
